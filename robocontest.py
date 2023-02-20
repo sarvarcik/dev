899c7d8f1f49127c5ecfc62f47619055d1c94601
sarvarrcik <124988916+sarvarrcik@users.noreply.github.com>
2023-02-20 16:45:50 +0500

    Update robocontest.py

--- robocontest.py
+++ robocontest.py
@@ -57,3 +57,36 @@ if n==1 or n==2:
   print(n)
 else:
   print(n*(n-1)*(n-2))
+
+  
+#0026
+mod = 1000000007
+t = int(input())
+x = []
+for i in range(t):
+    n = int(input())
+    x.append(n)
+for i in x:
+    print(i*i % mod)
+    
+    
+    
+#0027
+def top(n):
+    result = []
+    k, j = n-1, 0
+    for i in range(1,n+1):
+        if i%2 == 1:
+            result.append(k)
+            k -= 1
+        else:
+            result.append(j)
+            j += 1
+    return result
+t = int(input())
+x = []
+for i in range(t):
+    n,k = map(int, input().split())
+    x.append(top(n).index(k))
+for i in x:
+    print(i)
